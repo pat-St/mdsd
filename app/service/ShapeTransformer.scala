b3ccc50364a5ddde6b138da2c385194ba2f9ab9e
patrick sterk <patrick.sterk@protonmail.com>
2020-06-28 21:09:53 +0200

    fix bug

--- app/service/ShapeTransformer.scala
+++ app/service/ShapeTransformer.scala
@@ -5,18 +5,19 @@ import model._
 type EitherShape = Either[List[Shape],List[ErrorModel]]
 
 object ShapeTransformer:
-  //import service.ShapeErrorUtil
-  //import service.ShapeColorValidator
   def apply(parseResult: ErrorModel | List[Shape]): EitherShape = 
     parseResult match 
       case shape: List[Shape] => shape.validateColor
       case err: ErrorModel    => err
       match 
-      case shape: List[Shape] => shape.validateDim
-      case err: ErrorModel    => err
+      case shape: List[Shape | ErrorModel]  => sEu(shape)        
+      case error: ErrorModel                => Right(error)
+      match
+      case Left(shape) => shape.validateDim
+      case Right(err)    => err
       match 
       case shape: List[Shape | ErrorModel]  => sEu(shape)        
-      case error: ErrorModel                => Right(List(error))  
+      case error: ErrorModel                => Right(List(error))
     
   def (shapes: List[Shape]).validateColor
   (using validateColor: ShapeColorValidator)
